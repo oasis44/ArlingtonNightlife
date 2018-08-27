4e9296d30618e3dfd41a2f8dd2f32213078e5a4f
Mike Englert <michaelenglert44@hotmail.com>
2018-08-27 16:32:29 -0400

    Initial commit

---
+++ my-app.js
@@ -0,0 +1,38 @@
+// Dom7
+var $$ = Dom7;
+
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/about/',
+      url: 'about.html',
+    },
+  ]
+});
+
+var mainView = app.views.create('.view-main');
+
+/*
+$$('#events').on('click', function (e) {
+  console.log('changed');
+});
+*/
+
+$$(document.body).on('change', "select[name='eventSelect']", function(e){
+	console.log('the real change');
+	
+	Framework7.request.get('temp.html', function (data) {
+		console.log(data);
+	});
+});
