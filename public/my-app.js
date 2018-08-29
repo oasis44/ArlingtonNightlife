03acb6f99e4d81b283fcd2b1994853fd68ef7502
Mike Englert <michaelenglert44@hotmail.com>
2018-08-29 10:07:27 -0400

    Restructuring to support server side code

--- public/my-app.js
+++ public/my-app.js
@@ -23,16 +23,16 @@ var app = new Framework7({
 
 var mainView = app.views.create('.view-main');
 
-/*
-$$('#events').on('click', function (e) {
-  console.log('changed');
-});
-*/
-
 $$(document.body).on('change', "select[name='eventSelect']", function(e){
 	console.log('the real change');
 	
+	Framework7.request.get('/blah', function (data) {
+		console.log(data);
+	});
+	
+/*
 	Framework7.request.get('temp.html', function (data) {
 		console.log(data);
 	});
+*/
 });
