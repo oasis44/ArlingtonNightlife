39f80a96e7d3a0af0cebaf3d58e1df778b66d432
Mike Englert <michaelenglert44@hotmail.com>
2018-10-18 16:47:25 -0400

    Finished New Event page

--- server.js
+++ server.js
@@ -25,14 +25,24 @@ app.get('/events', function (req, res) {
 	});
 });
 
-app.post('/venues', function (req, res) { 
+app.post('/events', function (req, res) { 
 	const body = req.body
 	
-	const name = body.name
-	const address = body.address
-	const phone = body.phone
+	const eventType = body.eventType
+	const venueId = body.venue
+	const startTime = body.startTime
+	const endTime = body.endTime
 	
-	con.query("INSERT into venues (name, address, phone_num) VALUES (?, ?, ?)", [name, address, phone], function (err, result, fields) {
+	con.query("INSERT into events (type, start_time, end_time, venue_id) VALUES (?, STR_TO_DATE(?, '%M %e, %Y %T'), STR_TO_DATE(?, '%M %e, %Y %T'), ?)", [eventType, startTime, endTime, venueId], function (err, result, fields) {
+		if (err) throw err;
+		res.send(result);
+	});
+});
+
+app.get('/venues', function (req, res) { 
+	const venueId = req.params.id;
+
+	con.query("SELECT id, name FROM venues", function (err, result, fields) {
 		if (err) throw err;
 		res.send(result);
 	});
@@ -42,6 +52,19 @@ app.get('/venue/:id', function (req, res) {
 	const venueId = req.params.id;
 
 	con.query("SELECT name, address, phone_num FROM venues where id = ?", [venueId], function (err, result, fields) {
+		if (err) throw err;
+		res.send(result);
+	});
+});
+
+app.post('/venues', function (req, res) { 
+	const body = req.body
+	
+	const name = body.name
+	const address = body.address
+	const phone = body.phone
+	
+	con.query("INSERT into venues (name, address, phone_num) VALUES (?, ?, ?)", [name, address, phone], function (err, result, fields) {
 		if (err) throw err;
 		res.send(result);
 	});
