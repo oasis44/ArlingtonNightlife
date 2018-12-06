91f2d5e4ab408cc14665688be8555a2a5d9d71d7
Mike Englert <englert.mike@gmail.com>
2018-12-06 10:42:32 -0500

    Fix DB creation script

--- db_creation.sql
+++ db_creation.sql
@@ -1,4 +1,14 @@
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Iver4son3!'
+CREATE DATABASE arlington_nightlife;
+
+USE arlington_nightlife;
+
+CREATE TABLE `venues` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(45) NOT NULL,
+  `ADDRESS` varchar(100) NOT NULL,
+  `PHONE_NUM` varchar(12) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -8,12 +18,4 @@ CREATE TABLE `events` (
   `venue_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `venue_id` FOREIGN KEY (`id`) REFERENCES `venues` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `venues` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(45) NOT NULL,
-  `ADDRESS` varchar(100) NOT NULL,
-  `PHONE_NUM` varchar(12) NOT NULL,
-  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
